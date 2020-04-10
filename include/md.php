ed36c8091b8d112a8cf234038ef4c73965c20a60
Lucian Marin <marin.lucian@gmail.com>
2020-04-10 23:20:48 +0300

    tabs instead of spaces

--- include/md.php
+++ include/md.php
@@ -1,25 +1,25 @@
 <?php
-    function md($file) {
-        $text = '';
+	function md($file) {
+		$text = '';
 
-        foreach(explode("\n\n", $file) as $line) {
-            if (trim($line)) {
-                $text .= '<p>'.str_replace("\n", "<br>", trim($line)).'</p>';
-            }
-        }
+		foreach(explode("\n\n", $file) as $line) {
+			if (trim($line)) {
+				$text .= '<p>'.str_replace("\n", "<br>", trim($line)).'</p>';
+			}
+		}
 
-        $rules = array(
-            '/(\*\*|__)(.*?)\1/' => '<strong>\2</strong>',
-            '/(\*|_)(.*?)\1/' => '<em>\2</em>',
-            '/`(.*?)`/' => '<code>\1</code>',
-            '/!\[([^\[]+)\]\(([^\)]+)\)/' => '<img src=\'\2\' alt=\'\1\'>',
-            '/\[([^\[]+)\]\(([^\)]+)\)/' => '<a href="$2">$1</a>'
-        );
+		$rules = array(
+			'/(\*\*|__)(.*?)\1/' => '<strong>\2</strong>',
+			'/(\*|_)(.*?)\1/' => '<em>\2</em>',
+			'/`(.*?)`/' => '<code>\1</code>',
+			'/!\[([^\[]+)\]\(([^\)]+)\)/' => '<img src=\'\2\' alt=\'\1\'>',
+			'/\[([^\[]+)\]\(([^\)]+)\)/' => '<a href="$2">$1</a>'
+		);
 
-        foreach($rules as $regex => $replacement) {
-            $text = preg_replace($regex, $replacement, $text);
-        }
+		foreach($rules as $regex => $replacement) {
+			$text = preg_replace($regex, $replacement, $text);
+		}
 
-        return $text;
-    }
+		return $text;
+	}
 ?>
