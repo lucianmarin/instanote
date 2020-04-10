548ee14b6ccce8f98d4ec47944c49de0b97275b3
Lucian Marin <marin.lucian@gmail.com>
2020-04-10 23:52:47 +0300

    download apps from src.lm.com

--- products.php
+++ products.php
@@ -31,7 +31,7 @@
 				<img src="/images/monochrome.png" width="40" height="40" onclick="show('monochrome')">
 				<p>
 					<strong>Monochrome 5.2</strong> &mdash;
-					<a href="/apps/monochrome-5.2.dmg">download</a>
+					<a href="https://src.lucianmarin.com/apps/monochrome-5.2.dmg">download</a>
 				</p>
 				<p>Mobile browser for Mac.</p>
 				<p>Helps you design responsive sites and browse the mobile web from the comfort of your Mac.</p>
@@ -40,7 +40,7 @@
 				<img src="/images/multicolor.png" width="40" height="40" onclick="show('multicolor')">
 				<p>
 					<strong>Multicolor 1.1</strong> &mdash;
-					<a href="/apps/multicolor-1.1.dmg">download</a>
+					<a href="https://src.lucianmarin.com/apps/multicolor-1.1.dmg">download</a>
 				</p>
 				<p>Color schemes generator for Mac.</p>
 				<p>Generates five distinct colors and their luminance.</p>
@@ -49,7 +49,7 @@
 				<img src="/images/bicolor.png" width="40" height="40" onclick="show('bicolor')">
 				<p>
 					<strong>Bicolor 1.2</strong> &mdash;
-					<a href="/apps/bicolor-1.2.dmg">download</a>
+					<a href="https://src.lucianmarin.com/apps/bicolor-1.2.dmg">download</a>
 				</p>
 				<p>Contrast comparison tool for Mac.</p>
 				<p>Calculates the contrast ratio between two colors.</p>
