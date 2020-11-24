38ece48647b94ad2971d5596d6177d85c70c93d0
Lucian Marin <marin.lucian@gmail.com>
2020-11-24 03:29:23 +0200

    js for screenshots

--- include/header.php
+++ include/header.php
@@ -11,9 +11,7 @@
 	<link rel="alternate" type="application/rss+xml" title="Lucian Marin" href="/rss.php">
 	<link rel="manifest" type="application/json" href="/static/manifest.json">
 	<link rel="stylesheet" type="text/css" href="/static/style.css?v=24">
-	<?php if($auth): ?>
-		<script type="text/javascript" src="/static/script.js?v=9" async defer></script>
-	<?php endif; ?>
+	<script type="text/javascript" src="/static/script.js?v=9" async defer></script>
 </head>
 <?php if (strpos($self, 'edit')): ?>
 	<body onload="bodyLoad()">
