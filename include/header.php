23eec6d9d6cb16deaf3207e496dec2ba51aed096
Lucian Marin <marin.lucian@gmail.com>
2026-01-09 22:55:09 +0200

    fixed config

--- include/header.php
+++ include/header.php
@@ -6,8 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
 	<meta name="description" content="Python developer with Django and Pytest experience. Automation engineer and designer of human-computer interfaces.">
 	<meta name="author" content="Lucian Marin">
-	<link rel="apple-touch-icon" type="image/png" href="/images/plus-white.png">
-	<link rel="icon" sizes="192x192" type="image/png" href="/images/plus-white.png">
+	<link rel="apple-touch-icon" type="image/png" href="/static/images/plus-white.png">
+	<link rel="icon" sizes="192x192" type="image/png" href="/static/images/plus-white.png">
 	<link rel="alternate" type="application/rss+xml" title="Lucian Marin" href="/rss.php">
 	<link rel="manifest" type="application/json" href="/static/manifest.json">
 	<link rel="stylesheet" type="text/css" href="/static/style.css?v=29">
